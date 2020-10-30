d7d3a4c0c7cd382079448e7fd7bdca7b2d86c0e8
Marcos Cury <marcos.cury@fcamara.com.br>
2020-10-30 15:38:34 -0300

    fix: limiting axios max concurring requests

--- src/translate-api.js
+++ src/translate-api.js
@@ -11,6 +11,33 @@ const keepAliveAgent = new Agent({
 
 const axiosInstance = axios.create({ httpAgent: keepAliveAgent })
 
+const MAX_REQUESTS_COUNT = 20
+const INTERVAL_MS = 10
+let PENDING_REQUESTS = 0
+
+axiosInstance.interceptors.request.use(function (config) {
+  return new Promise((resolve, reject) => {
+    let interval = setInterval(() => {
+      if (PENDING_REQUESTS < MAX_REQUESTS_COUNT) {
+        PENDING_REQUESTS++
+        clearInterval(interval)
+        resolve(config)
+      }
+    }, INTERVAL_MS)
+  })
+})
+
+axiosInstance.interceptors.response.use(
+  function (response) {
+    PENDING_REQUESTS = Math.max(0, PENDING_REQUESTS - 1)
+    return Promise.resolve(response)
+  },
+  function (error) {
+    PENDING_REQUESTS = Math.max(0, PENDING_REQUESTS - 1)
+    return Promise.reject(error)
+  }
+)
+
 async function googleTranslate(source, target, apiKey, term) {
   const translateData = JSON.stringify({
     q: term,
